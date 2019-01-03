57d1e84d54b020811ea067b0a5e124d189c159d3
Kaylin Bittner <39201191+kaylinbittner@users.noreply.github.com>
2019-01-03 15:45:08 +0100

    Update ReverseAString.rb

--- ruby/ReverseAString.rb
+++ ruby/ReverseAString.rb
@@ -1,4 +1,10 @@
-#Write a function to reverse a string (w/o using .reverse!)
+#Write a function to reverse a string (w/o using .reverse! method)
 
-def reversed_string(string)
+def reverse_string(string)
+  string_split = string.split("")
+  reordered = []
+  string.size.times do
+    reordered << string_split.pop
+  end
+  reordered.join
 end
