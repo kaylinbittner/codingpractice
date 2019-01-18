8d8a1e40166e02fd4636a84ac6785b61fe6aded7
Kaylin Bittner <39201191+kaylinbittner@users.noreply.github.com>
2019-01-18 15:28:27 +0100

    Update ListMiddleElement.py

--- python/ListMiddleElement.py
+++ python/ListMiddleElement.py
@@ -1,4 +1,6 @@
-#If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+#If there are an odd number of elements in lst, the function should return the middle element. 
+#If there are an even number of elements, the function should return the average of the middle two elements.
+
 def middle_element(lst):
   if len(lst) % 2 == 0:
     sum = lst[int(len(lst)/2)] + lst[int(len(lst)/2) - 1]
