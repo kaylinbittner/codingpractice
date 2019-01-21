5e633cb533f2a1f25699bfec1fe55b3745d83324
Kaylin Bittner <39201191+kaylinbittner@users.noreply.github.com>
2019-01-21 14:02:30 +0100

    Create PoetryWhileLoop.py

---
+++ python/PoetryWhileLoop.py
@@ -0,0 +1,8 @@
+all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
+
+students_in_poetry = []
+
+while len(students_in_poetry) < 6:
+  students_in_poetry.append(all_students.pop())
+
+print(students_in_poetry)
