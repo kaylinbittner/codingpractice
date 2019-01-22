215b321875a94a2a46fe4328d0ffe8c7024d873b
Kaylin Bittner <39201191+kaylinbittner@users.noreply.github.com>
2019-01-22 16:08:18 +0100

    Create CommonLetters.py

---
+++ python/CommonLetters.py
@@ -0,0 +1,6 @@
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
