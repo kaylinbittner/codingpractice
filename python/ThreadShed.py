b73b504253f7df917221a491c19de7f89f2ee0f1
Kaylin Bittner <39201191+kaylinbittner@users.noreply.github.com>
2019-01-29 14:00:49 +0100

    Update ThreadShed.py

--- python/ThreadShed.py
+++ python/ThreadShed.py
@@ -105,8 +105,7 @@ green&white;,;09/15/17,   Gail Phelps   ;,;$30.52
 ;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
 ;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
 
-#------------------------------------------------
-# Start coding below!
+
 daily_sales_replaced = daily_sales.replace(';,;', '-')
 
 daily_transactions = daily_sales_replaced.split(',')
